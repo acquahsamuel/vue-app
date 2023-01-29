ad3875536b664ce6594475ec4d677ddba5b5b3c2
acquahsamuel <iamsamuelacquah@gmail.com>
2023-01-29 21:26:04 +0000

    assets base refactoring

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,16 +1,38 @@
 <script setup lang="ts">
-// import WelcomeItem from './WelcomeItem.vue'
-// import DocumentationIcon from './icons/IconDocumentation.vue'
-// import ToolingIcon from './icons/IconTooling.vue'
-// import EcosystemIcon from './icons/IconEcosystem.vue'
-// import CommunityIcon from './icons/IconCommunity.vue'
-// import SupportIcon from './icons/IconSupport.vue'
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+import Filter from './icons/filter.vue'
+import CustomerIcon from './icons/customer.vue'
+import SpinnerIcon from "./icons/spinner.svg";
+
 </script>
 
 <template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Documentation</template>
+
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <template #icon>
+      <!-- <Filter /> -->
+      <CustomerIcon />
+    </template>
+    <template #heading> Testing </template>
+
+  
+  </WelcomeItem>
+
  
 
-  <!-- <WelcomeItem>
+  <WelcomeItem>
     <template #icon>
       <SupportIcon />
     </template>
@@ -19,8 +41,10 @@
     As an independent project, Vue relies on community backing for its sustainability. You can help
     us by
     <a target="_blank" href="https://vuejs.org/sponsor/">becoming a sponsor</a>.
-  </WelcomeItem> 
- -->
- 
+
+  </WelcomeItem>
+
+
+
   
 </template>
